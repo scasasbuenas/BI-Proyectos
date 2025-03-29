aa73c994d5ac9ecab09f88bac95d1c9b9894f894
MrRobot138 <jp.castrob1@uniandes.edu.co>
2025-03-29 16:59:46 -0500

    fix

--- proyecto1/parte2/back/api/DataModel.py
+++ proyecto1/parte2/back/api/DataModel.py
@@ -5,7 +5,7 @@ class DataModel(BaseModel):
 # Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado.
     Titulo: str
     Descripcion: str
-    Label: str
+    Label: int
 
 #Esta función retorna los nombres de las columnas correspondientes con el modelo exportado en joblib.
     def columns(self):
--- proyecto1/parte2/back/api/main.py
+++ proyecto1/parte2/back/api/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
 
+
 app = FastAPI()
 
 # Add CORS middleware
